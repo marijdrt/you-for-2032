a8cf074059e1580da6c628d4d1a4898f37d96efe
marijdrt <marijdrt@gmail.com>
2022-01-25 19:49:01 +0700

    Final tweaks

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,5 +1,10 @@
 let mix = require('laravel-mix');
 
+mix.options({
+  // Don't perform any css url rewriting by default
+  processCssUrls: false,
+})
+
 mix.postCss("src/css/app.css", "public", [    
   require("tailwindcss"),  
 ]);
